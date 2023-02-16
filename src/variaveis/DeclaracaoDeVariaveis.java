1d0993d6a12c471bae2637a28b0fcb9a73a41d33
danietsuzuki <massao.tsuzuki@gmail.com>
2023-02-16 03:47:45 -0300

    Declaracao de variaveis

---
+++ src/variaveis/DeclaracaoDeVariaveis.java
@@ -0,0 +1,25 @@
+package variaveis;
+
+public class DeclaracaoDeVariaveis {
+
+	public static void main(String[] args) {
+		
+		int numero1 = 0, numero2 = 1;
+		System.out.println("Inteiro: " + (numero1 + numero2));
+		
+		double num1 = 0.5, num2 = 1.5;
+		System.out.println("Double: " + (num1 + num2));
+		
+		float numFlutuante = 0.5f;
+		System.out.println("Float: " + numFlutuante);
+		
+		char letra = 'B';
+		System.out.println("Char: " + letra);
+		
+		String palavra = "Cadeira";
+		System.out.println("String: " + palavra);
+		
+		
+	}
+
+}
