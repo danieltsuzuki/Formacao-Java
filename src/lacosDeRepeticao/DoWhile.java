632dfc94d8a9ca2ef6324809268283d85ea4e68d
danietsuzuki <massao.tsuzuki@gmail.com>
2023-02-17 02:35:05 -0300

    Estrutura de repeticao DoWhile

---
+++ src/lacosDeRepeticao/DoWhile.java
@@ -0,0 +1,26 @@
+package lacosDeRepeticao;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		int i = 0;
+		do {
+			System.out.println(i);
+			i++;
+		}while (i < 10);{
+			System.out.println(i);
+		}
+		
+		System.out.println("\n-----");
+		
+		int j = 10;
+		do {
+			System.out.println(j);
+			j--;
+		}while (j > 0); {
+			System.out.println(j);
+			
+		}
+	}
+
+}
