12c2af948f22d2834f7dcff9bf5bbcc2b4b64da8
danietsuzuki <massao.tsuzuki@gmail.com>
2023-02-17 02:29:31 -0300

    Estrutura de repeticao While

---
+++ src/lacosDeRepeticao/While.java
@@ -0,0 +1,21 @@
+package lacosDeRepeticao;
+
+public class While {
+
+	public static void main(String[] args) {
+		int i = 0;
+		while (i < 10) {
+			System.out.print(i + " ");
+			i++;
+		}
+		
+		System.out.println("\n------------");
+		
+		int j = 10;
+		while (j > 0) {
+			System.out.print(j + " ");
+			j--;
+		}
+	}
+
+}
